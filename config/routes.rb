957be725b3911d818bb9c4653dabb0cc8db25349
velajuel <ahmdfaiz@yahoo.com>
2017-07-05 16:10:54 +0800

    push to master

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 	root 'urls#index'
 	resources :urls
+	# the resources :urls is equivalent to:
+		# get '/urls' => 'urls#index'
+		# get '/urls/new' => 'urls#new'
+		# and so on
+		
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	get '/urls/:id/short' => 
+  	'urls#short'
+
 end
